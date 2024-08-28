66ec3e5337edacc5530e5929de768ccf41300805
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-08-28 12:24:04 +0600

    navbar added

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,236 @@
+import React, { useState } from "react";
+import logo from "../assets/logo-2.jpg";
+
+const NavBar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <div className="px-10 py-5 mx-auto sm:max-w-full md:max-w-full lg:max-w-screen md:px-24 lg:px-8 dark:bg-gray-900 text-white">
+      <div className="relative flex items-center justify-between">
+        <div className="flex items-center">
+          <a
+            href="/"
+            aria-label="Company"
+            title="Company"
+            className="inline-flex items-center mr-8"
+          >
+            <img src={logo} alt="logo" className="w-12 h-8 rounded-sm " />
+            <span className="ml-2 text-xl font-bold tracking-wide text-white uppercase">
+              Booking
+            </span>
+          </a>
+          <ul className="flex items-center hidden space-x-6 lg:flex mx-10">
+            <li>
+              <a
+                href="/"
+                aria-label="Our product"
+                title="Our product"
+                className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-label="Our product"
+                title="Our product"
+                className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                Facility
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-label="Product pricing"
+                title="Product pricing"
+                className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                Booking
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-label="Product pricing"
+                title="Product pricing"
+                className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                Admin
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-label="Product pricing"
+                title="Product pricing"
+                className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                User
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-label="About us"
+                title="About us"
+                className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-label="About us"
+                title="About us"
+                className="font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center md:order-2 space-x-3 ">
+          <div className="px-2  flex items-center space-x-2 ">
+            <img
+              className="w-8 h-8 rounded-full "
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU21DjrD8ExS5vLuTv5SAE1h9fJRjJP2OfSA&s"
+              alt="user photo"
+            />
+
+            <span className="block text-sm text-white">Bonnie Green</span>
+          </div>
+
+          <div className="flex space-x-2">
+            <button className="px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition duration-300">
+              Login
+            </button>
+            <button className="px-4 py-2 text-blue-600 bg-white border border-blue-600 rounded-lg hover:bg-blue-100 transition duration-300">
+              Register
+            </button>
+          </div>
+        </div>
+
+        <div className="lg:hidden md:hidden">
+          <button
+            aria-label="Open Menu"
+            title="Open Menu"
+            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+              />
+              <path
+                fill="currentColor"
+                d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+              />
+              <path
+                fill="currentColor"
+                d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+              />
+            </svg>
+          </button>
+          {isMenuOpen && (
+            <div className="absolute top-0 left-0 w-full">
+              <div className="p-5 bg-white border rounded shadow-sm">
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <a
+                      href="/"
+                      aria-label="Company"
+                      title="Company"
+                      className="inline-flex items-center"
+                    >
+                      <svg
+                        className="w-8 text-deep-purple-accent-400"
+                        viewBox="0 0 24 24"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeMiterlimit="10"
+                        stroke="currentColor"
+                        fill="none"
+                      >
+                        <rect x="3" y="1" width="7" height="12" />
+                        <rect x="3" y="17" width="7" height="6" />
+                        <rect x="14" y="1" width="7" height="6" />
+                        <rect x="14" y="11" width="7" height="12" />
+                      </svg>
+                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase"></span>
+                    </a>
+                  </div>
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                        <path
+                          fill="currentColor"
+                          d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <nav>
+                  <ul className="space-y-4">
+                    <li>
+                      <a
+                        href="/"
+                        aria-label="Our product"
+                        title="Our product"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="/"
+                        aria-label="Our product"
+                        title="Our product"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Facility
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="/"
+                        aria-label="Product pricing"
+                        title="Product pricing"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Booking
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="/"
+                        aria-label="About us"
+                        title="About us"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        About us
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/utils/utils.tsx
@@ -0,0 +1,23 @@
+<div className="flex items-center md:order-2 space-x-3 rtl:space-x-reverse">
+  <img
+    className="w-8 h-8 rounded-full "
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU21DjrD8ExS5vLuTv5SAE1h9fJRjJP2OfSA&s"
+    alt="user photo"
+  />
+  <div
+    className="z-50 my-2 text-base list-none divide-y divide-gray-100 rounded-lg shadow"
+    id="user-dropdown"
+  >
+    <div className="px-4 py-2">
+      <span className="block text-sm text-white">Bonnie Green</span>
+    </div>
+  </div>
+  <div className="flex space-x-2">
+    <button className="px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition duration-300">
+      Login
+    </button>
+    <button className="px-4 py-2 text-blue-600 bg-white border border-blue-600 rounded-lg hover:bg-blue-100 transition duration-300">
+      Registration
+    </button>
+  </div>
+</div>;
